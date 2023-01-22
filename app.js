550542a44de4407982bf70cc8041fd22de3237e1
Ashish jaswal <jaswalashish131@gmail.com>
2023-01-22 19:18:19 +0530

    list items added

--- app.js
+++ app.js
@@ -1,27 +1,36 @@
 
+const { request } = require('express');
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 app.set('view engine','ejs');
+const items = ["Buy Chines Noodles","New York is My new Home"]
 
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/',(req,res)=>{
     const today = new Date();
-     const current  = today.getDay();
-     const day = " ";
-    if(current===6 || current===0){
-        day="Fucking Day";
+    
+    const options = {
+        weekday:'long',
+        year:'numeric',
+        month:'long',
+        day:'numeric',
 
-        
-    }else{
-        day = "Work Day";
-      
-    }
-   res.render('list',{WhichDay:day})
+    };
+    
+    const day = today.toLocaleString('hi-IN',options);
+
+   res.render('list',{WhichDay:day,listitems:items});
 })
 
 app.listen(3000,function(){
     console.log('Listening on port 3000');
 })
 
-//Starting with ejs 266....
+//Post method
+app.post('/',(req,res)=>{
+    const item = req.body.data;
+    items.push(item);
+    res.redirect('/');
+})
